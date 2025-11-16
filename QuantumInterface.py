544f78b4de51faebadb119804d6459f423b93c9c
Hogrefe, Avery <avery.hogrefe@uconn.edu>
2025-11-16 18:35:33 -0500

    Update QuantumInterface.py

--- QuantumInterface.py
+++ QuantumInterface.py
@@ -9,8 +9,8 @@ class QuantumInterface:
         self._qc = QuantumCircuit(numQubits, numClassicalBits)
     
     def hadamard(self, start, end):
-        self.qc.h(range(start, end))
+        self._qc.h(range(start, end))
     
     def measure(self, start, end):
-        self.qc.measure(range(start, end), range(start, end))
+        self._qc.measure(range(start, end), range(start, end))
         # maybe have some return??
