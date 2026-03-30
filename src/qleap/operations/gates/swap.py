95ad2c9ceca5a0c929653d6056fc39c4cab458f9
Avery Hogrefe <145404812+WizardPizza72@users.noreply.github.com>
2026-03-30 18:45:07 -0400

    Add control option to swap
    Added option to control swap gate (cswap), needed for Shor's algorithm

--- src/qleap/operations/gates/swap.py
+++ src/qleap/operations/gates/swap.py
@@ -1,16 +1,24 @@
-from .gate import Gate
+from ._Gate import Gate
 from typing import override
 
 class Swap(Gate):
-    def __init__(self, q1, q2):
+    def __init__(self, q1, q2, control=None):
         super().__init__((q1, q2))
 
+        self.control = control
         if len(q1) > 1 or len(q2) > 1:
             raise ValueError("Swap targets should be single qubits")
         
+        if control is not None and len(control) > 1:
+            raise ValueError("Control should be a single qubit")
+
     @override
     def _apply(self, qi):
-        qi.swap(self._targets[0]._start, self._targets[1]._start)
+
+        if self.control is None:
+            qi.swap(self._targets[0]._start, self._targets[1]._start)
+        else:
+            qi.cswap(control, self._targets[0]._start, self._targets[1]._start)
 
     @override
     def _apply_inverse(self, qi):
